8aa403299997988f5e24410dad32f632041adf55
GeneCodeSavvy <hxrsh.09@gmail.com>
2026-04-30 08:56:51 +0530

    chore: configuring eslint

--- apps/web/eslint.config.js
+++ apps/web/eslint.config.js
@@ -1,22 +1,15 @@
-import js from '@eslint/js'
-import globals from 'globals'
-import reactHooks from 'eslint-plugin-react-hooks'
-import reactRefresh from 'eslint-plugin-react-refresh'
-import tseslint from 'typescript-eslint'
-import { defineConfig, globalIgnores } from 'eslint/config'
+import reactRefresh from "eslint-plugin-react-refresh";
+import { config as baseConfig } from "@repo/eslint-config/react-internal";
+import { defineConfig, globalIgnores } from "eslint/config";
 
 export default defineConfig([
-  globalIgnores(['dist']),
+  globalIgnores(["dist"]),
+  ...baseConfig,
   {
-    files: ['**/*.{ts,tsx}'],
-    extends: [
-      js.configs.recommended,
-      tseslint.configs.recommended,
-      reactHooks.configs.flat.recommended,
-      reactRefresh.configs.vite,
-    ],
-    languageOptions: {
-      globals: globals.browser,
+    files: ["**/*.{ts,tsx}"],
+    plugins: { "react-refresh": reactRefresh },
+    rules: {
+      ...reactRefresh.configs.vite.rules,
     },
   },
-])
+]);
