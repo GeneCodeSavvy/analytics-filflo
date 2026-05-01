fdc47bb287d8e676c36b0d4cfc505d88c6b3b9d7
GeneCodeSavvy <hxrsh.09@gmail.com>
2026-05-01 21:32:49 +0530

    feat: init the api

--- apps/api/src/index.ts
+++ apps/api/src/index.ts
@@ -1,1 +1,20 @@
-export {};
+import http from "http";
+import express from "express";
+import { WebSocketServer } from "ws";
+
+const PORT = 3000;
+
+const app = express();
+app.use(express.json());
+
+const server = http.createServer(app);
+
+export const wss = new WebSocketServer({ server });
+
+wss.on("connection", () => {
+  console.log("WebSocket client connected");
+});
+
+server.listen(PORT, () => {
+  console.log(`API + WS running on http://localhost:${PORT}`);
+});
