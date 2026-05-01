c3fe2c4d8a7f88837df656bf10af67bad1872d74
GeneCodeSavvy <hxrsh.09@gmail.com>
2026-05-01 12:25:00 +0530

    feat: add shared axios instance

---
+++ apps/web/src/api/index.ts
@@ -0,0 +1,12 @@
+import axios from "axios";
+
+export const api = axios.create({
+  baseURL: import.meta.env.VITE_API_BASE_URL,
+  withCredentials: true,
+  headers: { "Content-Type": "application/json" },
+});
+
+api.interceptors.response.use(
+  (res) => res,
+  (err) => Promise.reject(err),
+);
